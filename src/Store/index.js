871e30bf5bddab8c229e3cd34b098c6a20d414b2
unknown <odstmw@gmail.com>
2022-07-18 17:42:10 -0500

    Feature: Created Pinia store

--- src/Store/index.js
+++ src/Store/index.js
@@ -3,13 +3,31 @@ import { defineStore } from "pinia";
 export const useStore = defineStore("characters", {
   state: () => {
     return {
-      name: "Eduardo",
+      characters: [],
+      charactersCount: 0,
+      isLoading: true,
+      currentPage: 1,
     };
   },
   getters: {
     /* Getters */
   },
   actions: {
-    /* Actions */
+    setCharacters(characters) {
+      if (characters === null || characters === undefined) return;
+      this.characters = characters;
+    },
+    setCharactersCount(count) {
+      if (count === null || count === undefined) return;
+      this.charactersCount = count;
+    },
+    setIsLoading(value) {
+      if (value === null || value === undefined) return;
+      this.isLoading = value;
+    },
+    setCurrentPage(value) {
+      if (value === null || value === undefined) return;
+      this.currentPage = value;
+    },
   },
 });
