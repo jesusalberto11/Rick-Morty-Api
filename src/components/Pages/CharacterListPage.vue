1b752e53cf55b6989628043d16783a3d2bb17407
unknown <odstmw@gmail.com>
2022-07-18 18:15:37 -0500

    Update: Missing fields on component

--- src/components/Pages/CharacterListPage.vue
+++ src/components/Pages/CharacterListPage.vue
@@ -7,8 +7,8 @@
       <CharacterList @on-click-card="goToCharacter" :data="characters" />
     </div>
     <CharacterListPagination
-      @update-list="updateData"
-      :characterPages="charactersCount"
+      @update-list="fetchNewCharactersPage"
+      :characterPages="charactersPages"
     />
   </div>
 </template>
@@ -18,8 +18,13 @@ import { onMounted } from "vue";
 import { useRouter } from "vue-router";
 import { useCharacters } from "../../composable/useCharacters";
 
-const { characters, isLoading, charactersCount, getAllCharacters, updateData } =
-  useCharacters();
+const {
+  characters,
+  isLoading,
+  charactersPages,
+  getAllCharacters,
+  fetchNewCharactersPage,
+} = useCharacters();
 
 onMounted(() => {
   getAllCharacters();
