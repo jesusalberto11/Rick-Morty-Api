8d4523ff44e12be4b714120a9fd0b22a483ed41d
unknown <odstmw@gmail.com>
2022-07-18 18:16:22 -0500

    Feature: Added get character methods

--- src/composable/useCharacters.js
+++ src/composable/useCharacters.js
@@ -5,45 +5,84 @@ import { useStore } from "@/Store";
 export const useCharacters = () => {
   const charactersStore = useStore();
 
-  const { characters, isLoading, charactersCount } =
-    storeToRefs(charactersStore);
+  const {
+    character,
+    characters,
+    isLoading,
+    charactersPages,
+    firstEpisodeData,
+  } = storeToRefs(charactersStore);
 
   const ALL_CHARACTERS_API_URL = `https://rickandmortyapi.com/api/character/?page=1`;
+
+  /* Fetch al characters and setup the data */
 
   const getAllCharacters = () => {
     charactersStore.setIsLoading(true);
 
     axios
       .get(ALL_CHARACTERS_API_URL)
-      .then((response) => getData(response))
+      .then((response) => onCharactersDataResponse(response))
       .catch((error) => console.log("Error: ", error))
       .finally(() => charactersStore.setIsLoading(false));
   };
 
-  const getData = (response) => {
+  const onCharactersDataResponse = (response) => {
     charactersStore.setCharacters(response.data.results);
-    charactersStore.setCharactersCount(response.data.info.pages);
+    charactersStore.setCharactersPages(response.data.info.pages);
   };
 
-  const updateData = (id) => {
+  /* Update the characters list based on the page ID */
+
+  const fetchNewCharactersPage = (id) => {
     let newPageToFetch = `https://rickandmortyapi.com/api/character/?page=${id}`;
     charactersStore.setIsLoading(true);
 
     axios
       .get(newPageToFetch)
-      .then((response) => getData(response))
+      .then((response) => onCharactersDataResponse(response))
       .catch((error) => console.log("Error: ", error))
+      .finally(() => charactersStore.setIsLoading(false));
+  };
+
+  /* Fetch a single character based on this and ID, an fetch this first episode information */
+
+  const fetchSingleCharacterData = (characterId) => {
+    let API_URL = `https://rickandmortyapi.com/api/character/${characterId}`;
+    charactersStore.setIsLoading(true);
+
+    axios
+      .get(API_URL)
+      .then((response) => onSingleCharacterDataResponse(response))
+      .catch((error) => console.log("Error fetching character Data: ", error));
+  };
+
+  const onSingleCharacterDataResponse = (response) => {
+    charactersStore.setCharacter(response.data);
+    fetchFirstEpisode(response.data.episode[0]);
+  };
+
+  const fetchFirstEpisode = (firstEpisodeURL) => {
+    axios
+      .get(firstEpisodeURL)
+      .then((response) => charactersStore.setFirstEpisodeData(response.data))
+      .catch((error) =>
+        console.log("Error fetching First Episode Data: ", error)
+      )
       .finally(() => charactersStore.setIsLoading(false));
   };
 
   return {
     //! Properties
+    character,
     characters,
     isLoading,
-    charactersCount,
+    charactersPages,
+    firstEpisodeData,
 
     //! Methods
     getAllCharacters,
-    updateData,
+    fetchNewCharactersPage,
+    fetchSingleCharacterData,
   };
 };
