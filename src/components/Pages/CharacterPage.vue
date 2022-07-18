6ff8b0f88c762a41194563645b81f5f428220f43
unknown <odstmw@gmail.com>
2022-07-18 18:14:52 -0500

    Refractor: Refractor: Added composition API + Pinia

--- src/components/Pages/CharacterPage.vue
+++ src/components/Pages/CharacterPage.vue
@@ -5,15 +5,29 @@
     </div>
     <div v-else>
       <CharacterDetailedCard
-        :characterData="characterData"
-        :firstSeenEpisodeData="firstSeenEpisodeData"
+        :characterData="character"
+        :firstSeenEpisodeData="firstEpisodeData"
       />
     </div>
   </div>
 </template>
 
+<script setup>
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+import { useCharacters } from "../../composable/useCharacters";
+
+const { character, isLoading, firstEpisodeData, fetchSingleCharacterData } =
+  useCharacters();
+
+const route = useRoute();
+
+onMounted(() => {
+  fetchSingleCharacterData(route.params.id);
+});
+</script>
+
 <script>
-import axios from "axios";
 import CharacterDetailedCard from "../CharacterDetails/CharacterDetailedCard.vue";
 import SkeletonDetailedCard from "../Skeletons/SkeletonDetailedCard.vue";
 
@@ -22,42 +36,6 @@ export default {
   components: {
     CharacterDetailedCard,
     SkeletonDetailedCard,
-  },
-  data() {
-    return {
-      API_URL: "https://rickandmortyapi.com/api/character/2",
-      characterData: null,
-      isLoading: true,
-      firstSeenEpisodeData: null,
-    };
-  },
-  mounted() {
-    this.fetchData();
-  },
-  methods: {
-    fetchData() {
-      let API_URL = `https://rickandmortyapi.com/api/character/${this.$route.params.id}`;
-
-      axios
-        .get(API_URL)
-        .then((response) => this.onResponse(response))
-        .catch((error) =>
-          console.log("Error fetching character Data: ", error)
-        );
-    },
-    onResponse(response) {
-      this.characterData = response.data;
-      this.fetchFirstEpisode(response.data.episode[0]);
-    },
-    fetchFirstEpisode(firstEpisodeURL) {
-      axios
-        .get(firstEpisodeURL)
-        .then((response) => (this.firstSeenEpisodeData = response.data))
-        .catch((error) =>
-          console.log("Error fetching First Episode Data: ", error)
-        )
-        .finally(() => (this.isLoading = false));
-    },
   },
 };
 </script>
