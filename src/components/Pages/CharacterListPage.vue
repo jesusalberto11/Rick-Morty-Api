36e42aee575caa4dc713af0a42da5384da4866fc
Jesus Alberto <58407784+jesusalberto11@users.noreply.github.com>
2022-07-18 19:05:25 -0500

    Revert "Refractor: Added composition API + Pinia"
    This reverts commit 401ad84473114b64a2e33675b370c54b77105b26.

--- src/components/Pages/CharacterListPage.vue
+++ src/components/Pages/CharacterListPage.vue
@@ -4,38 +4,17 @@
       <SkeletonList />
     </div>
     <div v-else>
-      <CharacterList @on-click-card="goToCharacter" :data="characters" />
+      <CharacterList @on-click-card="goToCharacter" :data="data" />
     </div>
     <CharacterListPagination
       @update-list="updateData"
-      :characterPages="charactersCount"
+      :characterPages="characterCount"
     />
   </div>
 </template>
 
-<script setup>
-import { onMounted } from "vue";
-import { useRouter } from "vue-router";
-import { useCharacters } from "../../composable/useCharacters";
-
-const { characters, isLoading, charactersCount, getAllCharacters, updateData } =
-  useCharacters();
-
-onMounted(() => {
-  getAllCharacters();
-});
-
-const router = useRouter();
-
-const goToCharacter = (characterId) => {
-  router.push({
-    name: "character",
-    params: { id: characterId },
-  });
-};
-</script>
-
 <script>
+import axios from "axios";
 import CharacterList from "../CharacterList/CharacterList.vue";
 import CharacterListPagination from "../PagesComponents/CharacterListPagination.vue";
 import SkeletonList from "../CharacterList/SkeletonList.vue";
@@ -46,6 +25,44 @@ export default {
     CharacterList,
     CharacterListPagination,
     SkeletonList,
+  },
+  data() {
+    return {
+      API_URL: `https://rickandmortyapi.com/api/character/?page=1`,
+      data: null,
+      isLoading: true,
+      currentPage: 1,
+      characterCount: null,
+    };
+  },
+  mounted() {
+    axios
+      .get(this.API_URL)
+      .then((response) => this.getData(response))
+      .catch((error) => console.log("Error: ", error))
+      .finally(() => (this.isLoading = false));
+  },
+  methods: {
+    getData(response) {
+      this.data = response.data.results;
+      this.characterCount = response.data.info.pages;
+    },
+    updateData(id) {
+      let newPageToFetch = `https://rickandmortyapi.com/api/character/?page=${id}`;
+      this.isLoading = true;
+
+      axios
+        .get(newPageToFetch)
+        .then((response) => this.getData(response))
+        .catch((error) => console.log("Error: ", error))
+        .finally(() => (this.isLoading = false));
+    },
+    goToCharacter(characterId) {
+      this.$router.push({
+        name: "character",
+        params: { id: characterId },
+      });
+    },
   },
 };
 </script>
