ce9066926cc586191108521401b4774c84fd8490
Jesus Alberto <58407784+jesusalberto11@users.noreply.github.com>
2022-07-18 19:05:25 -0500

    Revert "Feature: Added new states"
    This reverts commit 0be758863181d8d4292daec18e56a9a21177e0ae.

--- src/Store/index.js
+++ src/Store/index.js
@@ -3,29 +3,23 @@ import { defineStore } from "pinia";
 export const useStore = defineStore("characters", {
   state: () => {
     return {
-      character: {},
       characters: [],
-      charactersPages: 0,
+      charactersCount: 0,
       isLoading: true,
       currentPage: 1,
-      firstEpisodeData: {},
     };
   },
   getters: {
     /* Getters */
   },
   actions: {
-    setCharacter(character) {
-      if (character === null || character === undefined) return;
-      this.character = character;
-    },
     setCharacters(characters) {
       if (characters === null || characters === undefined) return;
       this.characters = characters;
     },
-    setCharactersPages(count) {
+    setCharactersCount(count) {
       if (count === null || count === undefined) return;
-      this.charactersPages = count;
+      this.charactersCount = count;
     },
     setIsLoading(value) {
       if (value === null || value === undefined) return;
@@ -34,10 +28,6 @@ export const useStore = defineStore("characters", {
     setCurrentPage(value) {
       if (value === null || value === undefined) return;
       this.currentPage = value;
-    },
-    setFirstEpisodeData(data) {
-      if (data === null || data === undefined) return;
-      this.firstEpisodeData = data;
     },
   },
 });
