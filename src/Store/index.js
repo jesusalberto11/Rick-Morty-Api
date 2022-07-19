0978ab3603d4a5d2554ad192823d7cc5ef176eee
Jesus Alberto <58407784+jesusalberto11@users.noreply.github.com>
2022-07-18 19:05:25 -0500

    Revert "Feature: Created Pinia store"
    This reverts commit 060ac2a8973e525f4ffa9eb35f8f354acda2118f.

--- src/Store/index.js
+++ src/Store/index.js
@@ -3,31 +3,13 @@ import { defineStore } from "pinia";
 export const useStore = defineStore("characters", {
   state: () => {
     return {
-      characters: [],
-      charactersCount: 0,
-      isLoading: true,
-      currentPage: 1,
+      name: "Eduardo",
     };
   },
   getters: {
     /* Getters */
   },
   actions: {
-    setCharacters(characters) {
-      if (characters === null || characters === undefined) return;
-      this.characters = characters;
-    },
-    setCharactersCount(count) {
-      if (count === null || count === undefined) return;
-      this.charactersCount = count;
-    },
-    setIsLoading(value) {
-      if (value === null || value === undefined) return;
-      this.isLoading = value;
-    },
-    setCurrentPage(value) {
-      if (value === null || value === undefined) return;
-      this.currentPage = value;
-    },
+    /* Actions */
   },
 });
